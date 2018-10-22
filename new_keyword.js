f7e2e31279cd6b396e59f05cbe792ca92d013b65
Leo Tejeda <leonelhtejeda@gmail.com>
2018-10-22 07:22:31 -0700

    this with prototype

--- new_keyword.js
+++ new_keyword.js
@@ -108,3 +108,26 @@ Person.prototype.sayHi = function(){
 
 elie = new Person("Elie");
 elie.sayHi();// Hi Elie
+
+
+
+function Vehicle(make,model,year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.isRunning = false;
+}
+
+Vehicle.prototype.turnOn = function(){
+    this.isRunning = true;
+}
+
+Vehicle.prototype.turnOff = function(){
+    this.isRunning = false;
+}
+
+Vehicle.prototype.honk = function(){
+    if(this.isRunning){
+        return "beep";
+    }
+}
